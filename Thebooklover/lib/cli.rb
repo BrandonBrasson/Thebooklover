aaa93c8ef0236a8d5e73a723f0699a26fd117a63
Brandon Brasson <rocksong622@gmail.com>
2018-10-09 04:28:33 +0000

    Add existing file

--- Thebooklover/lib/cli.rb
+++ Thebooklover/lib/cli.rb
@@ -2,7 +2,6 @@ class The_Keeper::CLI
 
 
    def call
-     binding.pry
      The_Keeper::Book.scrape_storyindexone
     list_storybook
     menu
@@ -10,31 +9,30 @@ class The_Keeper::CLI
   end
 
       def list_storybook
-    # here doc - "https://www.imdb.com/title/tt0078346/plotsummary?ref_=tt_stry_pl"
         puts "THE KEEPER OF TIME"
         @book = The_Keeper::Book.all
         @book.each.with_index(1) do |book, i|
-         puts "#{i}.#{book.name} "
+         puts "#{i}.#{book.name}"
      end
    end
 
 
 
-      def menu
-        input = nil
-        while input != "exit"
-          puts "list of data"
-          input = gets.strip.downcase
+    def menu
+      input = nil
+    while input != "exit"
+        puts "list of books"
+        input = gets.strip.downcase
 
-      if input.to_i > 0
-        the_book = @book[input.to_i-1]
-        puts " #{the_book.name} - #{the_book.price} - #{the_book.url}"
-      elsif input == "list"
-        list_storybook
-      else
-        puts "Not sure what you want, type list or exit."
-      end
+    if input.to_i > 0
+      the_book = @book[input.to_i-1]
+      puts " #{the_book.name} - #{the_book.price} - #{the_book.url}"
+    elsif input == "list"
+      list_storybook
+    else
+      puts "Not sure what you want, type list or exit."
     end
+  end
 end
 
   def goodbye
--- Thebooklover/lib/scraper.rb
+++ Thebooklover/lib/scraper.rb
@@ -2,61 +2,28 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-
 class The_Keeper::Book
-attr_accessor   :url, :name, :price
+attr_accessor :url, :name, :price
 
   @@all = []
 
   def initialize
-    @@all << self #self == Book instance
+    @@all << self
   end
 
   def self.all
-      # Scrape storyindexone and storyindextwo and then return deals based on that data
-    @@all
-    binding.pry
+   @@all
   end
 
-    #def self.scrape_book
-    #  book = []
-#
-    #  book << self.scrape_storyindexone
-#   #   book << self.scrape_storyindextwo
-
-    #  book
-    #end
-
     def self.scrape_storyindexone
-
-    #  book = doc.search("li.col-xs-6")
-      #book.map do |book|
-      #book_name = doc.search("h3 a").text
-    #  book_price = doc.search("p.price_color").text
-
-
-
       doc = Nokogiri::HTML(open("http://books.toscrape.com/catalogue/category/books/travel_2/index.html"))
       list_doc = doc.css("li.col-xs-6")
-     list_doc.each do |element|
-        #binding.pry
-        book = self.new
-       book.name = element.css("h3 a").text
-        book.price = element.css("p.price_color").text
-         book.url = ("http://books.toscrape.com/catalogue/category/books/travel_2/index.html")
+      list_doc.each do |element|
+          book = self.new
+          book.name = element.css("h3 a").text
+          book.price = element.css("p.price_color").text
+          book.url = ("http://books.toscrape.com/catalogue/category/books/travel_2/index.html")
       end
     end
 
-
-  #  def self.scrape_storyindexone
-      #doc = Nokogiri::HTML(open("http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"))
-      #list_doc = doc.css("head")
-      #list_doc.each do |element|
-      #  binding.pry
-        #book = self.new
-        #book.name = doc.search("h1")
-        #book.url = element.css('p').text
-    #end
-#end
-
-end
+ end
