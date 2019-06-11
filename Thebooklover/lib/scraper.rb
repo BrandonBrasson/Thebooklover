32ecd4bcac286588272c62de3ff43a68935cdd9e
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2019-06-11 19:02:40 -0400

    Update scraper.rb

--- Thebooklover/lib/scraper.rb
+++ Thebooklover/lib/scraper.rb
@@ -4,7 +4,6 @@ class The_Keeper::Scraper
       doc = Nokogiri::HTML(open("http://books.toscrape.com/catalogue/category/books/travel_2/index.html"))
       list_doc = doc.css("li.col-xs-6")
       list_doc.each do |element|
-       #binding.pry
           book = The_Keeper::Book.new
           book.name = element.css("h3 a").text
           book.price = element.css("p.price_color").text
